c3e8cffa3c427acbb80e7f08eaa09ff0bbb9f5f3
LostButStillImproving <70686136+LostButStillImproving@users.noreply.github.com>
2020-12-03 20:35:51 +0100

    removed prototype from main

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module org.openjfx {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires org.junit.jupiter.api;
-    opens org.openjfx to javafx.fxml;
-    opens org.openjfx.model to javafx.base;
-    exports org.openjfx;
-}
