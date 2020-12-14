1a2714b359c6593bb4c03ed8f2d30b7a7994fc23
LostButStillImproving <70686136+LostButStillImproving@users.noreply.github.com>
2020-12-14 11:36:41 +0100

    added update()

--- src/view/Observer.java
+++ src/view/Observer.java
@@ -1,4 +1,9 @@
 package view;
 
+import control.RiskManagerController;
+import exceptions.NoProjectException;
+
 public interface Observer {
+
+    public void Update(RiskManagerController controller) throws NoProjectException;
 }
