6ccf6f03921f93773c9749b707b123d67f7a64c6
Anders Hjordrup Larsen <70882629+Hjordrup@users.noreply.github.com>
2020-12-14 12:45:57 +0100

    added risk

--- src/model/Risk.java
+++ src/model/Risk.java
@@ -1,7 +1,8 @@
-package model;
+import model.CounterMeasure;
 
-public class Risk {
+import java.io.Serializable;
 
+public class Risk implements Serializable {
 
     //Datafield (variables)
     private String riskName;
@@ -12,33 +13,97 @@ public class Risk {
     private double revisedProbability;
     private double revisedConsequence;
     private CounterMeasure cm;
-
+    static int count = 0;
+    int id;
 
 
     // Constructor
-    public Risk(String riskName, double probability, double consequence, String description){
+    public Risk(String riskName, double probability, double consequence, String description) {
         this.riskName = riskName;
         this.consequence = consequence;
-        this.priority = probability;
+        this.probability = probability;
         this.description = description;
+        this.priority = consequence * probability;
+        this.id = count++;
     }
-
-
 
     //Methods
-    public void addCounterMeasure(double probabilityImpact, double consequenceImpact, String description, boolean active){
-        cm =  new CounterMeasure(probabilityImpact,consequenceImpact,description,active);
+    public void addCounterMeasure(double probabilityImpact, double consequenceImpact, String description, boolean active) {
+        cm = new CounterMeasure(probabilityImpact, consequenceImpact, description, active);
+        if (cm.isActive()) {
+            updatePriority();
+        }
     }
 
-    public void activateCounterMeasure(boolean wantedState){
+    public void activateCounterMeasure(boolean wantedState) {
+        cm.activeCounterMeasure(wantedState);
+        if (cm.isActive()) {
+            updatePriority();
+        }
+    }
 
-
+    public void updatePriority() {
+        if (cm == null) {
+            return;
+        } else {
+            this.revisedConsequence = cm.getConsequenceImpact() * this.consequence;
+            this.revisedProbability = cm.getProbabilityImpact() * this.probability;
+            this.priority = revisedConsequence * revisedProbability;
+        }
     }
 
 
+    public void editRisk(String riskName, double probability, double consequence, String description) {
+
+        this.description = description;
+        this.probability = probability;
+        this.consequence = consequence;
+        this.riskName = riskName;
+        this.priority = consequence * probability;
+        updatePriority();
+    }
+
+    public void removeCounterMeasure() {
+        this.cm = null;
+    }
 
 
+    // Getters and Setters
+    public String getRiskName() {
+        return riskName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public double getConsequence() {
+        return consequence;
+    }
+
+    public double getProbability() {
+        return probability;
+    }
+
+    public double getPriority() {
+        return priority;
+    }
+
+    public double getRevisedConsequence() {
+        return revisedConsequence;
+    }
+
+    public double getRevisedProbability() {
+        return revisedProbability;
+    }
+
+    public CounterMeasure getCm() {
+        return cm;
+    }
+
+    public int getID() {
+        return id;
+    }
 
 
-  
 }
