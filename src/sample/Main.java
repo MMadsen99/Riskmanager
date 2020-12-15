41d52678c52ffc114ee906ac4e80088b48df73d8
Anders Hjordrup Larsen <70882629+Hjordrup@users.noreply.github.com>
2020-12-15 14:09:03 +0100

    Made Main work with load function

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,30 +1,46 @@
 package sample;
-
 import javafx.application.Application;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
-public class Main extends Application {
 
-    static Scene scene;
+import java.io.*;
+
+/**
+ * JavaFX App
+ */
+public class Main extends Application {
+    private static Scene scene;
+    private static FileChooser fileChooser = new FileChooser();
+
     public static Scene getScene() {
         return scene;
     }
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("RiskManager");
-        primaryStage.setScene(new Scene(root, 1, 1));
-        primaryStage.setMaximized(true);
-        primaryStage.show();
+    public void start(Stage stage) throws IOException {
+        scene = new Scene(loadFXML("primary"), 640, 480);
+        stage.setScene(scene);
+        stage.setTitle("RiskManager");
+        stage.show();
+        stage.setMaximized(true);
     }
 
+    static void setRoot(String fxml) throws IOException {
+        scene.setRoot(loadFXML(fxml));
+    }
 
+    private static Parent loadFXML(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource( "sample.fxml"));
+
+        return fxmlLoader.load();
+    }
 
     public static void main(String[] args) {
-        launch(args);
+        launch();
     }
 }
