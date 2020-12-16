a8fc97700c46e8f98f42cd1ed60e1f3b70807242
Anders Hjordrup Larsen <70882629+Hjordrup@users.noreply.github.com>
2020-12-16 12:06:31 +0100

    Added all popups to the program.

--- src/control/RiskManagerController.java
+++ src/control/RiskManagerController.java
@@ -2,10 +2,17 @@ package control;
 
 import exceptions.NoProjectException;
 import exceptions.NoRiskException;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TableView;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 import model.Project;
 import model.ProjectTable;
 import persistence.ProjectLibrary;
@@ -14,12 +21,33 @@ import view.ProjectsFX;
 import view.RiskFX;
 import view.RiskTableFX;
 
+
+import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class RiskManagerController implements Initializable {
+    @FXML
+    private Button createProject;
 
+    @FXML
+    private Button  loadProject;
+
+    @FXML
+    private Button ModifyRiskKnap;
+
+    @FXML
+    private Button addCounterMeasure;
+
+    @FXML
+    private Button activateCounterMeasureKnap;
+
+    @FXML
+    private Button deleteKnap;
+
+    @FXML
+    private Button addRisk;
 
     @FXML
     public ListView<ProjectsFX> projectFXListView;
@@ -106,4 +134,50 @@ public class RiskManagerController implements Initializable {
         RiskManagerController riskManagerController = new RiskManagerController();
         ProjectLibrary.saveProjects(riskManagerController);
     }
+
+   public void displayAddRiskPopUp(ActionEvent event) throws IOException {
+           Stage window = new Stage();
+           Parent addRiskParent = FXMLLoader.load(RiskManagerController.class.getResource("../sample/AddRisk.fxml"));
+           Scene addRiskScene = new Scene(addRiskParent, 500, 300);
+           //Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+           window.initModality(Modality.APPLICATION_MODAL);
+           window.setScene(addRiskScene);
+           window.showAndWait();
+    }
+
+    public void displayCreateProjectPopUp(ActionEvent event) throws IOException {
+        Stage window = new Stage();
+        Parent createProjectParent = FXMLLoader.load(RiskManagerController.class.getResource("../sample/CreateProject.fxml"));
+        Scene createProjectScene = new Scene(createProjectParent, 500, 300);
+        //Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.initModality(Modality.APPLICATION_MODAL);
+        window.setScene(createProjectScene);
+        window.showAndWait();
+    }
+
+    public void displayModifyRiskPopUp(ActionEvent event) throws IOException {
+        Stage window = new Stage();
+        Parent modifyRiskParent = FXMLLoader.load(RiskManagerController.class.getResource("../sample/ModifyRisk.fxml"));
+        Scene modifyRiskScene = new Scene(modifyRiskParent, 500, 300);
+        //Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.initModality(Modality.APPLICATION_MODAL);
+        window.setScene(modifyRiskScene);
+        window.showAndWait();
+    }
+
+    public void displayAddCounterMeasurePopUp(ActionEvent event) throws IOException {
+        Stage window = new Stage();
+        Parent addCounterMeasureParent = FXMLLoader.load(RiskManagerController.class.getResource("../sample/AddCounterMeasure.fxml"));
+        Scene addCounterMeasureScene = new Scene(addCounterMeasureParent, 500, 300);
+        //Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.initModality(Modality.APPLICATION_MODAL);
+        window.setScene(addCounterMeasureScene);
+        window.showAndWait();
+    }
+
+
+
+
+
+
 }
